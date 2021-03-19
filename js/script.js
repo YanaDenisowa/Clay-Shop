babc1b8275b6dea7e32c18bf34a09488d9846e81
yana <yanags1990@gmail.com>
2021-03-19 22:56:54 +0200

    Fixing pagination

--- js/script.js
+++ js/script.js
@@ -18,18 +18,21 @@ $(document).ready(function(){
                 breakpoint:992,
                 settings: {
                     slidesToShow:3,
+                    slidesToScroll:3,
                 }
             },
             {
                 breakpoint:768,
                 settings: {
                     slidesToShow:2,
+                    slidesToScroll:2,
                 }
             },
             {
                 breakpoint:576,
                 settings: {
                     slidesToShow:1,
+                    slidesToScroll:1,
                 }
             }
 
@@ -63,12 +66,14 @@ $(document).ready(function(){
                 breakpoint:768,
                 settings: {
                     slidesToShow:2,
+                    slidesToScroll:2,
                 }
             },
             {
                 breakpoint:576,
                 settings: {
                     slidesToShow:1,
+                    slidesToScroll:1,
                 }
             }
 
